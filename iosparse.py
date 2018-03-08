34a1864c623dabc0dd25d983e25f9cd78e6d5ab5
Heath Kuespert <hrk601@gmail.com>
2018-03-08 10:20:23 -0600

    updates

--- iosparse.py
+++ iosparse.py
@@ -1,15 +1,14 @@
-# cfgFile = open('netcopa/completed_configurations/711.txt')
-
-#port number / legacy vlan / orgin switchname
-
 #validate that voice =/= data
 
 import re
 import sys
+import os
 
-pathin = sys.argv[1]
-file = open(pathin, "r")
-outputfile = "netcopa/completed_configurations/711-new.txt"
+inread = os.path.abspath(sys.argv[1])
+
+pathin = open(inread, "r")
+file = pathin
+outputfile = "testout.txt"
 switch = file
 gigCount = 0
 hostname = ""
@@ -20,76 +19,81 @@ hostnameFinal = ""
 
 def switchread(switchtype):
     if switchtype == "gig":
+        
+        pathin.seek(0)
+        s = file
+        
         with open(outputfile, 'a') as csvfile:
-            #switch.close()
-            filehere = open(pathin, "r")
-            s = filehere
-            print("os: cisco_ios\nvars:\n  interfaces:")
+            # switch.close()
+            # filehere = open(pathin, "r")
+            print("os: cisco_ios\nvars:\n   hostname: "+hostnameFinal+"\n   interfaces:")
             for gigLine in s:
                 checkgigint = re.compile("(Gi.*0/[1-9][0-9]|Gi.*0/[1-9]|Fa.*0/[1-9][0-9]|Fa*0/[1-9])")
                 if checkgigint:
                     matchgigresult = checkgigint.search(gigLine)
                     if matchgigresult:
-                        print("    ",matchgigresult.group(0),":\n      name: ", matchgigresult.group(0), "\n      switchport:")
+                        print("    ",matchgigresult.group(0),":\n      name: "+matchgigresult.group(0), "\n      switchport:")
 
                 checkaccessvlan = re.compile("switchport access vlan (.*)")
                 if checkaccessvlan:
                     matchaccessresult = checkaccessvlan.search(gigLine)
                     if matchaccessresult:
-                        # print("Here: ", matchgigresult.group(0))
-                        print("        access:\n          vlan: ", matchaccessresult.group(1))
+                        # print("Here: "+matchgigresult.group(0))
+                        print("        access:\n          vlan: "+matchaccessresult.group(1))
 
                 checkportmoderegex = re.compile("switchport mode access")
                 if checkportmoderegex:
                     matchaccessresult = checkportmoderegex.search(gigLine)
                     if matchaccessresult:
-                        # print("Here: ", matchgigresult.group(0))
+                        # print("Here: "+matchgigresult.group(0))
                         print("        mode:\n        - access")
 
                 checkvoicevlan = re.compile("switchport voice vlan (.*)")
                 if checkvoicevlan:
                     matchvoiceresult = checkvoicevlan.search(gigLine)
                     if matchvoiceresult:
-                        # print("Here: ", matchgigresult.group(0))
-                        print("        voice:\n          vlan: ", matchvoiceresult.group(1))
+                        # print("Here: "+matchgigresult.group(0))
+                        print("        voice:\n          vlan: "+matchvoiceresult.group(1))
 
     if switchtype == "fe":
         with open(outputfile, 'a') as csvfile:
             # exportwriter = csv.writer(csvfile, delimiter=',')
             # exportwriter.writerow(['Switch Name', 'Interface', 'Address Pool(VN)', 'Voice Pool(VN)', 'Authentication'])
             #switch.close()
-            filehere = open(pathin, "r")
-            s = filehere
-            print("os: cisco_ios\nvars:\n  interfaces:")
+            pathin.seek(0)
+            s = file
+            #filehere = open(pathin, "r")
+            #s = filehere
+            print("os: cisco_ios\nvars:\n   hostname: "+hostnameFinal+"\n   interfaces:")
             for gigLine in s:
                 checkgigint = re.compile("(Fa.*0/[1-9][0-9]|Fa.*0/[1-9])")
                 if checkgigint:
                     matchgigresult = checkgigint.search(gigLine)
                     if matchgigresult:
 
-                        print("    ", matchgigresult.group(0), ":\n      name: ", matchgigresult.group(0),
+                        print("    "+matchgigresult.group(0)+":\n      name: "+matchgigresult.group(0),
                               "\n      switchport:")
 
                 checkaccessvlan = re.compile("switchport access vlan (.*)")
                 if checkaccessvlan:
                     matchaccessresult = checkaccessvlan.search(gigLine)
                     if matchaccessresult:
-                        # print("Here: ", matchgigresult.group(0))
-                        print("        access:\n          vlan: ", matchaccessresult.group(1))
+                        # print("Here: "+matchgigresult.group(0))
+                        print("        access:\n          vlan: "+matchaccessresult.group(1))
 
                 checkportmoderegex = re.compile("switchport mode access")
                 if checkportmoderegex:
                     matchaccessresult = checkportmoderegex.search(gigLine)
                     if matchaccessresult:
-                        # print("Here: ", matchgigresult.group(0))
+                        # print("Here: "+matchgigresult.group(0))
                         print("        mode:\n        - access")
 
                 checkvoicevlan = re.compile("switchport voice vlan (.*)")
                 if checkvoicevlan:
                     matchvoiceresult = checkvoicevlan.search(gigLine)
                     if matchvoiceresult:
-                        # print("Here: ", matchgigresult.group(0))
-                        print("        voice:\n          vlan: ", matchvoiceresult.group(1))
+                        # print("Here: "+matchgigresult.group(0))
+                        print("        voice:\n          vlan: "+matchvoiceresult.group(1))
 
                 checkbadint = re.compile("interface GigabitEthernet0*")
                 if checkbadint:
@@ -98,7 +102,7 @@ def switchread(switchtype):
                         break
 
 
-                    #exportwriter.writerow([hostnameFinal, matchgigresult.group(0), "1", "1", "1"])
+                    #exportwriter.writerow([hostnameFinal, matchgigresult.group(0), "1"+"1"+"1"])
 
 for line in switch:
     checkHostName = re.compile("hostname\s*")
@@ -116,10 +120,11 @@ for line in switch:
             gigCount = gigCount + 1
 
 #print(gigCount)
-if gigCount > 6:
-    switchread("gig")
-if gigCount <= 6:
+if gigCount < 24:
     switchread("fe")
+else:
+    switchread("gig")
+
 
 
 #print(gigCount)
